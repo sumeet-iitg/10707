8a3557ad5fe33e7a433487c799818a891f8c5c4a
sumeet-iitg <sumeet.iitg@gmail.com>
2019-02-12 10:00:37 -0500

    Implementation pass 0

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 import numpy as np
 import os
 import sys
-from .data_loader import load_data
+from data_loader import load_data
 
 
 class Activation(object):
@@ -157,11 +157,11 @@ class SoftmaxCrossEntropy(Criterion):
         # exponents = np.exp(self.logits - np.max(self.logits))
 
         # assuming batch is the 0th dimension
-        self.sm = exponents/(1+np.sum(exponents, axis=1))
-        self.sm = self.sm.squeeze(axis=2) # bsz x label_size x 1 --> bsz x label_size
+        self.sm = exponents.squeeze(axis=2)/(1+np.sum(exponents, axis=1)) # bsz x label_size x 1 --> bsz x label_size
+
         # cross entropy for entire batch matrix
-        x_entropy_loss = -np.sum(np.dot(np.log(self.sm), y))
-        return x_entropy_loss
+        x_entropy_loss = -np.sum(np.dot(np.log(self.sm), y.T))
+        return self.sm, x_entropy_loss
 
     def derivative(self):
 
@@ -225,12 +225,12 @@ class BatchNorm(object):
 
 
 # These are both easy one-liners, don't over-think them
-def random_normal_weight_init(bsz, d0, d1):
-    return np.random.random((bsz, d0, d1))
+def random_normal_weight_init(d0, d1):
+    return np.random.random((d0, d1))
 
 
-def zeros_bias_init(d, bsz):
-    raise np.zeros(d, bsz)
+def zeros_bias_init(d):
+    return np.zeros(d)
 
 
 class MLP(object):
@@ -265,17 +265,17 @@ class MLP(object):
         prev_layer_size = self.input_size
         for layer_idx, layer_size in enumerate(hiddens):
             # because we are left multiplying W with X_in and H thereafter.
-            self.W.append(weight_init_fn(batch_size, hiddens[layer_idx], prev_layer_size))
-            self.dW.append(np.zeros(batch_size, hiddens[layer_idx], prev_layer_size))
-            self.b.append(bias_init_fn(batch_size, prev_layer_size))
-            self.db.append(np.zeros(batch_size, prev_layer_size))
+            self.W.append(weight_init_fn(hiddens[layer_idx], prev_layer_size))
+            self.dW.append(np.zeros((hiddens[layer_idx], prev_layer_size)))
+            self.b.append(bias_init_fn(hiddens[layer_idx]))
+            self.db.append(np.zeros(hiddens[layer_idx]))
             prev_layer_size = layer_size
 
         # weights for output layer
-        self.W.append(weight_init_fn(batch_size, output_size, hiddens[-1]))
-        self.dW.append(np.zeros(batch_size, output_size, hiddens[-1]))
-        self.b.append(bias_init_fn(batch_size, hiddens[-1]))
-        self.db.append(np.zeros(batch_size, hiddens[-1]))
+        self.W.append(weight_init_fn(output_size, hiddens[-1]))
+        self.dW.append(np.zeros((output_size, hiddens[-1])))
+        self.b.append(bias_init_fn(output_size))
+        self.db.append(np.zeros(output_size))
 
         self.stored_activations = []
 
@@ -289,17 +289,18 @@ class MLP(object):
         # activate the weight matrices
         bsz = x.shape[0]
         X_curr = x
-        n_hidden = len(self.W)
 
         self.stored_activations.append(X_curr)
-        for layer_idx in range(0, n_hidden):
+        for layer_idx in range(0, self.nlayers-1):
             X_prev = X_curr # bsz x nhid[l-1]
-            X_prev = np.reshape(X_prev, (bsz, 1, -1)) # bsz x 1 x nhid[l-1]
-            # W = bsz x nhid[l] x nhid[l-1] | X.T = bsz x nhid[l-1] x 1
-            lin_comb = np.matmul(self.W[layer_idx], X_prev.transpose([0,2,1])) + self.b[layer_idx] # bsz x nhid[l] x 1
-            X_curr = self.activations[layer_idx].forward(lin_comb).squeeze(axis=2) # bsz x nhid[l]
+            # X_prev = np.reshape(X_prev, (bsz, 1, -1)) # bsz x 1 x nhid[l-1]
+            # W = nhid[l] x nhid[l-1] | X.T = nhid[l-1] x bsz
+            lin_comb = np.matmul(self.W[layer_idx], X_prev.T).T + self.b[layer_idx] #  bsz x nhid[l]
+            X_curr = self.activations[layer_idx].forward(lin_comb) # bsz x nhid[l]
             # saving input and activated input for gradient computation
             self.stored_activations.append(X_curr)
+        # output layer
+        X_curr = np.matmul(self.W[-1], X_curr.T).T + self.b[-1]  # bsz x out_size
 
         return X_curr
 
@@ -309,8 +310,8 @@ class MLP(object):
     def step(self):
 
         for layer_idx in range(0, self.nlayers):
-            self.W[layer_idx] -= self.lr * self.dW[layer_idx]
-            self.b[layer_idx] -= self.lr * self.db[layer_idx]
+            self.W[layer_idx] -= self.lr * (self.dW[layer_idx]/32.0)
+            self.b[layer_idx] -= self.lr * (self.db[layer_idx]/32.0)
 
     def backward(self, labels):
         bsz = labels.shape[0]
@@ -322,28 +323,32 @@ class MLP(object):
         h = self.stored_activations[-1] # [bsz x 100]
         h = np.expand_dims(h, axis=1) # [bsz x 1 x 100]
         # dL/dW = dL/do[bsz x label_size x 1] * do/dW [bsz x 1 x 100]
-        self.dW[-1] = np.matmul(dAct, h) # [bsz x label_size x 100]
-        # dL/db = dL/do x I
+        grads = np.matmul(dAct, h) # [bsz x label_size x 100]
+        self.dW[-1] = np.sum(grads, axis=0) # sum along batch dim
+        # dL/db = dL/do
         dOut_prev = dAct.squeeze(axis=2)
-        self.db[-1] = dOut_prev
+        self.db[-1] = np.sum(dOut_prev,axis=0)
 
-        W_prev = self.W[-1] #[bsz x nhid[l] x nhid[l-1]]
+        W_prev = self.W[-1] #[nhid[l] x nhid[l-1]]
 
         for layer_idx in reversed(range(0,self.nlayers-1)):
-            dOut_prev = np.expand_dims(dOut_prev, axis=1)
-            # dL/dh = dL/do x do/dh = dl/do x W_prev = [bsz x nhid[l+1]] x [bsz x nhid[l+1] x nhid[l]]
-            dAct_curr = np.matmul(dOut_prev, W_prev) # [bsz x 1 x nhid[l]]
+            # dL/dh = dL/do x do/dh = dl/do x W_prev = [bsz x nhid[l+1]] x [nhid[l+1] x nhid[l]]
+            # ==> [[nhid[l+1] x nhid[l]].T x [bsz x nhid[l+1]]
+            dAct_curr = np.matmul(W_prev.T, dOut_prev.T).T # [bsz x nhid[l]]
+
             # dh/dq
-            dAct_Out = self.activations[layer_idx].derivative() # bsz x nhid[l] x nhid[l]
+            dAct_Out = self.activations[layer_idx].derivative() # bsz x nhid[l]
             # dL/dq = dL/dh x dh/dq
-            dLoss_Out = np.matmul(dAct_curr, dAct_Out) # bsz x 1 x nhid[l]
+            dLoss_Out = np.multiply(dAct_curr, dAct_Out) # bsz x nhid[l]
+            dLoss_Out = np.expand_dims(dLoss_Out, axis=1) # bsz x 1 x nhid[l]
             # dq/dW
             h = self.stored_activations[layer_idx] # bsz x nhid[l-1]
             h = np.expand_dims(h, axis=1) # bsz x 1 x nhid[l-1]
             # dL/dW = dL/dq x dq/dW
-            self.dW[layer_idx] = np.matmul(dLoss_Out.transpose([0,2,1]), h) # bsz x nhid[l] x nhid[l-1]
+            grads = np.matmul(dLoss_Out.transpose([0,2,1]), h) # bsz x nhid[l] x nhid[l-1]
+            self.dW[layer_idx] = np.sum(grads, axis=0)
             # dL/db = dL/dq
-            self.db[layer_idx] = dLoss_Out.squeeze(axis=1)
+            self.db[layer_idx] = np.sum(dLoss_Out.squeeze(axis=1), axis=0)
             dOut_prev = dLoss_Out
             W_prev = self.W[layer_idx]
 
@@ -383,13 +388,13 @@ def get_training_stats(mlp, dset, nepochs, batch_size):
         train_loss = 0
         for b in range(0, len(trainx), batch_size):
             # inputs are row vectors batch_size x 1568
-            X_in = trainx[prev_b:b]
+            X_in = np.asarray(trainx[b:b+batch_size])
             # labels batch_size x 19
-            labels = trainy[prev_b:b]
+            labels = np.asarray(trainy[b:b+batch_size])
             logits = mlp(X_in) # bsz x out_size
-            train_loss += mlp.criterion.forward(logits, labels)
+            sfmax, loss = mlp.criterion.forward(logits, labels)
+            train_loss += loss
             mlp.backward(labels)
-            prev_b = b
             print("Training loss: {}".format(float(train_loss/batch_size)))
             training_losses.append(train_loss)
 
@@ -411,8 +416,6 @@ def get_training_stats(mlp, dset, nepochs, batch_size):
     # Return results ...
 
     # return (training_losses, training_errors, validation_losses, validation_errors)
-
-    raise NotImplemented
 
 if __name__ == '__main__':
     directory_path = "C:\\Users\\SumeetSingh\\Documents\\Lectures\\10-707\\HW-Code\\split_data_problem_5_hw1"
